231325bdc8ed2dc090befb3f8565b6ee72d0469a
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-04-03 07:38:38 +0530

    [Nikhil] Add: Finding repeated words in a sentence.

---
+++ src/main/java/com/bridgelabz/regex/RepeatedWords.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.regex;
+
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RepeatedWords {
+    public static void main(String[] args) {
+        String text = "This is is a repeated repeated word test";
+
+        String regex = "\\w+";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        HashSet<String> set = new HashSet<>();
+        while(matcher.find()){
+            if(set.contains(matcher.group())){
+                System.out.println(matcher.group());
+            }
+            else{
+                set.add(matcher.group());
+            }
+        }
+    }
+}

282b5797eddc08e0ec2794d83883ab5656e837dc
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-04-03 07:15:42 +0530

    [Nikhil] Mod: modified extractdates

--- src/main/java/com/bridgelabz/regex/ExtractDates.java
+++ src/main/java/com/bridgelabz/regex/ExtractDates.java
@@ -7,13 +7,12 @@ import java.util.regex.Pattern;
 public class ExtractDates {
 
     public static void Dates(String str){
-        Pattern pattern = Pattern.compile("[0-9]{2}/[0-9]{2}/[0-9]{4}");
+        Pattern pattern = Pattern.compile("\\b(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/[0-9]{4}\\b");
         Matcher matcher = pattern.matcher(str);
 
         while(matcher.find()){
             System.out.print(matcher.group(0) + ", ");
         }
-
     }
     public static void main(String[] args) {
 
